3a6e1eec4b2e1a46a765bc42acda1de3cade3fa1
JuanCarlosCalleja <35761299+juancaresp@users.noreply.github.com>
2023-03-14 17:50:22 +0100

    InicioSpring
    Creacion del pryecto de Spring

---
+++ Spring/boup/src/test/java/com/boup/boup/BoupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boup.boup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
