8fead3a30e5b3825f2122220a5def7cbb0e20d6a
Carlos Molina Arcediano <carlos16298@gmail.com>
2023-04-10 15:42:02 +0200

    creacion de interfaces

---
+++ AppAndroid/AppBoup/app/src/main/java/es/boup/appboup/listaUsuariosElegirPagador.java
@@ -0,0 +1,24 @@
+package es.boup.appboup;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class listaUsuariosElegirPagador extends Fragment {
+
+
+    public listaUsuariosElegirPagador() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.lista_usuarios_elegir_pagador, container, false);
+    }
+}
