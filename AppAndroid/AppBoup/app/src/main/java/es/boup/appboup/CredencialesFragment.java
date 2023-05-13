72881b85af11d6597821265a30a859162643cd87
Enriquess <enriquess842@gmail.com>
2023-05-13 16:02:56 +0200

    Completar registro usuarios

--- AppAndroid/AppBoup/app/src/main/java/es/boup/appboup/CredencialesFragment.java
+++ AppAndroid/AppBoup/app/src/main/java/es/boup/appboup/CredencialesFragment.java
@@ -19,7 +19,7 @@ import android.widget.Toast;
 
 public class CredencialesFragment extends Fragment {
 
-    private EditText etNombre,etUsername,etTelefono;
+    private EditText etNombre,etTelefono;
     private TextView tvTelefono,tvNombre;
     private Button boton;
     //recoger el bundle del fragmento de inicio
@@ -40,55 +40,33 @@ public class CredencialesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         etNombre = view.findViewById(R.id.etNombreC);
-        etUsername = view.findViewById(R.id.etUsernameC);
         etTelefono = view.findViewById(R.id.etTelefonoC);
         tvTelefono = view.findViewById(R.id.tvTelefonoC);
         tvNombre = view.findViewById(R.id.tvNombreC);
+
         boton = view.findViewById(R.id.botonC);
-
-        boton.setOnClickListener((view1)->{
-            //hacer llamada a la api para introducir el usuario en nuestra base de datos propia
-            //
-            //
-            //Reemplazar el fragmento actual con el nuevo fragmento
-            if (correo){
-                if (!etTelefono.getText().toString().isEmpty() && !etNombre.getText().toString().isEmpty() && !etUsername.getText().toString().isEmpty()) {
-                    FragmentManager fragmentManager = getParentFragmentManager();
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.frame, new listaInicio())
-                            .addToBackStack(null)
-                            .commit();
-                }else{
-                    Toast.makeText(getActivity(), "Faltan campos por rellenar", Toast.LENGTH_SHORT).show();
-                }
-            }else{
-                if (!etUsername.getText().toString().isEmpty()) {
-                    FragmentManager fragmentManager = getParentFragmentManager();
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.frame, new listaInicio())
-                            .addToBackStack(null)
-                            .commit();
-                }else{
-                    Toast.makeText(getActivity(), "Faltan campos por rellenar", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-        });
+        boton.setOnClickListener((view1)->confirmar());
 
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getParentFragmentManager().setFragmentResultListener("resultadoRegistro", this, (requestKey, result) -> {
-            if (!result.getBoolean("correo")){
-                etTelefono.setVisibility(View.GONE);
-                etNombre.setVisibility(View.GONE);
-                tvNombre.setVisibility(View.GONE);
-                tvTelefono.setVisibility(View.GONE);
-                etTelefono.setVisibility(View.GONE);
-                correo = false;
-            }
-        });
+    private void confirmar(){
+        Bundle bundle = new Bundle();
+        String nombre,telefono;
+        nombre = etNombre.getText().toString();
+        telefono = etTelefono.getText().toString();
+        //Reemplazar el fragmento actual con el nuevo fragmento
+        if (!telefono.isEmpty() && !nombre.isEmpty()) {
+            bundle.putString("telefono",telefono);
+            bundle.putString("nobmre",nombre);
+            FragmentManager fragmentManager = getParentFragmentManager();
+            getParentFragmentManager().setFragmentResult("resultadoCredenciales",bundle);
+            fragmentManager.beginTransaction()
+                    .replace(R.id.frame, new RegistroFragment())
+                    .addToBackStack(null)
+                    .commit();
+        }else{
+            Toast.makeText(getActivity(), "Rellena los campos vacios", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
---
+++ AppAndroid/AppBoup/app/src/main/java/es/boup/appboup/Services/MyFirebaseMessagingService.java
@@ -0,0 +1,14 @@
+package es.boup.appboup.Services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+    }
+}
