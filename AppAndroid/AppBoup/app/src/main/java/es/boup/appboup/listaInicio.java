542c250de489bd1178fa8170e5861b90709968ef
Carlos Molina Arcediano <carlos16298@gmail.com>
2023-05-15 17:24:32 +0200

    caso de uso crear grupo

--- AppAndroid/AppBoup/app/src/main/java/es/boup/appboup/listaInicio.java
+++ AppAndroid/AppBoup/app/src/main/java/es/boup/appboup/listaInicio.java
@@ -6,15 +6,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class listaInicio extends Fragment {
 
-    private Button boton;
+    private Button btnVisibilizar;
+    private EditText etCrearGrupo;
+    private Button btnCrearGrupo;
+
 
     public listaInicio() {
         // Required empty public constructor
@@ -26,14 +32,33 @@ public class listaInicio extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lista_inicio, container, false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        etCrearGrupo=view.findViewById(R.id.etCrearGrupo);
+        btnCrearGrupo=view.findViewById(R.id.btnCrearGrupo);
+        btnVisibilizar=view.findViewById(R.id.btnVisibilizar);
+        btnVisibilizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etCrearGrupo.setVisibility(View.VISIBLE);
+                btnCrearGrupo.setVisibility(View.VISIBLE);
+            }
+        });
 
-
+        btnCrearGrupo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //llamar a endpoint de crearGrupo con el et.gettext
+            }
+        });
     }
+
+
+
 
 }
 
