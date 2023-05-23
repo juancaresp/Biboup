2b4eb3de716de2e9dbb1f8b0b4d3cadb05f0c732
Carlos Molina Arcediano <carlos16298@gmail.com>
2023-05-23 20:13:51 +0200

    add(addParticipante): call to endpoint

--- AppAndroid/AppBoup/app/src/main/java/es/boup/appboup/Services/IGroupService.java
+++ AppAndroid/AppBoup/app/src/main/java/es/boup/appboup/Services/IGroupService.java
@@ -5,10 +5,17 @@ import es.boup.appboup.Model.Group;
 import es.boup.appboup.Model.User;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface IGroupService {
     @POST("api/groups/add/{groupname}/user/{username}")
     Call<Group> insertarUsuario(@Path("groupname") String groupname,@Path("username") String username);
+
+    @GET("api/groups/{id}")
+    Call<Group> getGroupById(@Path("id") String id);
+
+    @POST("api/groups/{idgroup}/addUser/{username}")
+    Call<Group> insertarUsuarioEnGrupo(@Path("idgroup") int idgroup,@Path("username") String username);
 }
