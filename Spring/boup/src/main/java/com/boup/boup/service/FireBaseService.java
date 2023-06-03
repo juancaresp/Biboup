a3693bf9de34fb3511e4d9541c5ba300b57a2fdc
JuanCarlosCalleja <35761299+juancaresp@users.noreply.github.com>
2023-06-03 20:37:00 +0200

    ADD- Notificaciones a la api a falta de archivo de login

---
+++ Spring/boup/src/main/java/com/boup/boup/service/FireBaseService.java
@@ -0,0 +1,37 @@
+package com.boup.boup.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.boup.boup.dto.FireBaseNot;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
+@Service
+public class FireBaseService {
+
+	@Autowired FirebaseMessaging fireBase;
+	
+	public boolean sendNotification(FireBaseNot notification) {
+		boolean exit=false;
+		
+		Notification not=Notification.builder()
+				.setTitle(notification.getTittle())
+				.setBody(notification.getBody())
+				.setImage(notification.getImage())
+				.build();
+		Message mes=Message.builder()
+				.setToken(notification.getToken())
+				.setNotification(not)
+				.build();
+		try {
+			fireBase.send(mes);
+			exit=true;
+		}catch (FirebaseMessagingException e) {
+			e.printStackTrace();
+		}
+		return exit;
+	}
+}
